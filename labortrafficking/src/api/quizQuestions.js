224f525510725cc543993a5d4e9ca1cf3b18ccd8
Abhinav Sathiamoorthy <abhisat@bu.edu>
2018-07-01 22:53:41 -0400

    Sample questionnaire giving yes, no or maybe

---
+++ labortrafficking/src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "First question in Category 1",
+      answers: [
+          {
+              type: "Yes",
+              content: "Answer1"
+          },
+          {
+              type: "No",
+              content: "Answer2"
+          },
+          {
+              type: "Maybe",
+              content: "Answer3"
+          }
+      ]
+  },
+  {
+      question: "Second question in Category 1",
+      answers: [
+          {
+              type: "Yes",
+              content: "Answer1"
+          },
+          {
+              type: "No",
+              content: "Answer2"
+          },
+          {
+              type: "Maybe",
+              content: "Answer3"
+          }
+      ]
+  },
+  {
+      question: "Third question in Category 1",
+      answers: [
+          {
+              type: "Yes",
+              content: "Answer1"
+          },
+          {
+              type: "No",
+              content: "Answer2"
+          },
+          {
+              type: "Maybe",
+              content: "Answer3"
+          }
+      ]
+  },
+  {
+      question: "Fourth question in Category 1",
+      answers: [
+          {
+              type: "Yes",
+              content: "Answer1"
+          },
+          {
+              type: "No",
+              content: "Answer2"
+          },
+          {
+              type: "Maybe",
+              content: "Answer3"
+          }
+      ]
+  },
+  {
+      question: "Fifth question in Category 1",
+      answers: [
+          {
+              type: "Yes",
+              content: "Answer1"
+          },
+          {
+              type: "No",
+              content: "Answer2"
+          },
+          {
+              type: "Maybe",
+              content: "Answer3"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
